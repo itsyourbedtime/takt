5f95dc35001f1a102e997e443abff06c978f7b2e
its your bedtime <you@example.com>
2020-02-22 02:09:41 +0700

    fix

--- lib/browser.lua
+++ lib/browser.lua
@@ -1,9 +1,5 @@
 local fs = {}
-
-
-
--- fileselect utility
--- reroutes redraw/enc/key
+-- fileselect mod
 
 
 function fs.enter(folder, callback, id)
@@ -32,8 +28,7 @@ function fs.enter(folder, callback, id)
 end
 
 function fs.exit()
-  if (fs.path and fs.callback) then fs.callback(fs.sample_id, fs.path) end
-  --else fs.callback("cancel") end
+  if (fs.path and fs.callback) then fs.callback(fs.sample_id, fs.path) params:set('play_mode_' .. fs.sample_id, 2) end
   fs.open = false
   engine.noteOff(100)
 end
@@ -44,7 +39,6 @@ fs.getdir = function()
   for k,v in pairs(fs.folders) do
     path = path .. v
   end
-  --print("path: "..path)
   return path
 end
 
@@ -60,8 +54,7 @@ fs.getlist = function()
   -- Generate display list and lengths
   for k, v in ipairs(fs.list) do
     local line = v
-    local max_line_length = 80
-
+    local max_line_length = 76
     line = util.trim_string_to_width(line, max_line_length)
     fs.display_list[k] = line
   end
@@ -78,7 +71,7 @@ fs.preview_sample = function(z)
             path = path .. v
             end
             fs.path = path .. fs.file
-
+            fs.preview = true
             params:set("sample_100", fs.path)
             -- small delay to get it all working
             local m  = metro.init()
@@ -86,41 +79,36 @@ fs.preview_sample = function(z)
             m.count = 1
             m.event = function()
                 engine.playMode(100, 2)
-                engine.noteOn(100, 261.6, 1, 100 )
+                engine.noteOn(100, 261.6, 0.7, 100 )
             end
             m:start()
         end
     elseif z == 0 then
         engine.noteOff(100)
-
+        fs.preview = false
     end
 end
 
 fs.key = function(n,z)
   if n == 1 then
     fs.preview_sample(z)
-    -- back
   elseif n==2 and z==1 then
     if fs.depth > 0 then
-      --print('back')
       fs.folders[fs.depth] = nil
       fs.depth = fs.depth - 1
       fs.getlist()
-    --fs.redraw()
     else
+      fs.path = nil
       fs.exit()
-      fs.done = true
     end
     -- select
   elseif n==3 and z==1 then
     if #fs.list > 0 then
       fs.file = fs.list[fs.pos+1]
       if string.find(fs.file,'/') then
-        --print("folder")
         fs.depth = fs.depth + 1
         fs.folders[fs.depth] = fs.file
         fs.getlist()
-        --fs.redraw()
       else
         local path = fs.folder
         for k,v in pairs(fs.folders) do
@@ -130,8 +118,8 @@ fs.key = function(n,z)
         fs.done = true
       end
     end
-  end
-  if z == 0 and fs.done == true then
+  
+  elseif z == 0 and fs.done == true then
     fs.exit()
   end
 end
@@ -151,54 +139,26 @@ fs.enc = function(n,d)
   if n==2 then
     fs.pos = util.clamp(fs.pos + d, 0, fs.len - 1)
     fs.s_offset = fs.update_offset(fs.s_offset, fs.pos+1, fs.len, fs.bounds_y, 2)
-    print(fs.s_offset, fs.len, fs.pos )
-    --fs.redraw()
   end
 end
 
 
 fs.redraw = function()
-  if #fs.list == 0 then
-    screen.level(4)
-    screen.move(45, 15)
-    screen.text("(no files)")
-  else
---[[    for i=1,6 do
-      if (i > 2 - fs.pos) and (i < fs.len - fs.pos + 3) then
-        local list_index = i+fs.pos-2
-        screen.move(46, 8 +  8 * i)
-        if(i==3) then
-          screen.level(9)
-        else
-          screen.level(2)
-        end
-        screen.text(fs.display_list[list_index])
+    if #fs.list == 0 then
+        screen.level(4)
+        screen.move(45, 15)
+        screen.text("(no files)")
+    else
+        for i= 1, fs.bounds_y do 
+            local list_index = i + (fs.s_offset)
 
+            screen.level(4)
+            screen.move(46, 8 + 8 * i )
+            screen.level(fs.pos + 1 == list_index and 9 or 2)
+            screen.text(fs.display_list[list_index] or '')
+            screen.stroke()
         end
     end
-]]  
-
-for i= 1, fs.bounds_y do 
-
-    screen.level(4)
-    screen.move(46, 8 + 8 * i )
-    local list_index = i + (fs.s_offset)
-    
-    if( fs.pos + 1  == list_index ) then
-        screen.level(9)
-      else
-        screen.level(2)
-      end
-    screen.text(fs.display_list[list_index] or '')
-
-    screen.stroke()
-  end
-
-
-
-
-end
-  ---screen.update()
 end
 
 
